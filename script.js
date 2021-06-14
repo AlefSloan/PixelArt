7a1a4d52ef051ac1afad50ed48d1d9d018d4810d
Alef Sloan <Alef_sloan@hotmail.com>
2021-06-14 09:27:30 -0300

    Requisitos 10, 11 e 12 finalizados. Lint respeitado

--- script.js
+++ script.js
@@ -1,104 +1,96 @@
+const pixels = document.getElementsByTagName('td');
+const eraseBtn = document.getElementById('clear-board');
+const boardInput = document.getElementById('board-size');
+const boardBtn = document.getElementById('generate-board');
+
 function turnBlackSelected() {
   const colorPalette1 = document.getElementById('color-palette');
   colorPalette1.firstElementChild.classList.add('selected');
 }
 
-window.onload = turnBlackSelected;
+function turnSelectedColor(event) {
+  const selectedList = document.getElementsByClassName('selected')[0];
+  if (event.target.classList.contains('selected')) {
+    // empty
+  } else {
+    selectedList.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+
+function randomColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
 
 const colorPalette = document.getElementById('color-palette');
-const cores = ['black', 'yellow', 'blue', 'red'];
+const cores = ['black', randomColor(), randomColor(), randomColor()];
 for (let index = 0; index < 4; index += 1) {
   const div = document.createElement('div');
   div.className = 'color';
   div.style.backgroundColor = cores[index];
+  div.addEventListener('click', turnSelectedColor);
   colorPalette.appendChild(div);
 }
 
-const pixelBoard = document.getElementById('pixel-board');
-for (let index = 0; index < 5; index += 1) {
-  const tRow = document.createElement('tr');
-  pixelBoard.appendChild(tRow);
-  for (let index2 = 0; index2 < 5; index2 += 1) {
-    const tDiv = document.createElement('td');
-    tDiv.className = 'pixel';
-    tDiv.style.backgroundColor = 'white';
-    pixelBoard.children[index].appendChild(tDiv);
+function removeLastBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
+  while (pixelBoard.children[0]) {
+    pixelBoard.removeChild(pixelBoard.lastElementChild);
   }
 }
-
-const colors = document.getElementsByClassName('color');
-const colorBlack = colors[0];
-const colorYellow = colors[1];
-const colorBlue = colors[2];
-const colorRed = colors[3];
-
-function turnSelectedBlack(event) {
-  if (event.target.classList.contains('selected') === true) {
-    // empty
-  } if (event.target.classList.contains('selected') === false) {
-    colorYellow.classList.remove('selected');
-    colorBlue.classList.remove('selected');
-    colorRed.classList.remove('selected');
-    colorBlack.classList.add('selected');
-  }
-}
-
-function turnSelectedBlue(event) {
-  if (event.target.classList.contains('selected') === true) {
-    // empty
-  } if (event.target.classList.contains('selected') === false) {
-    colorBlack.classList.remove('selected');
-    colorYellow.classList.remove('selected');
-    colorRed.classList.remove('selected');
-    colorBlue.classList.add('selected');
-  }
-}
-
-function turnSelectedRed(event) {
-  if (event.target.classList.contains('selected') === true) {
-    // empty
-  } if (event.target.classList.contains('selected') === false) {
-    colorBlack.classList.remove('selected');
-    colorYellow.classList.remove('selected');
-    colorBlue.classList.remove('selected');
-    colorRed.classList.add('selected');
-  }
-}
-
-function turnSelectedYellow(event) {
-  if (event.target.classList.contains('selected') === true) {
-    // empty
-  } if (event.target.classList.contains('selected') === false) {
-    colorBlack.classList.remove('selected');
-    colorBlue.classList.remove('selected');
-    colorRed.classList.remove('selected');
-    colorYellow.classList.add('selected');
-  }
-}
-
-colorBlack.addEventListener('click', turnSelectedBlack);
-colorRed.addEventListener('click', turnSelectedRed);
-colorYellow.addEventListener('click', turnSelectedYellow);
-colorBlue.addEventListener('click', turnSelectedBlue);
-
-const pixels = document.getElementsByTagName('td');
 
 function turnSameColor(event) {
+  const clickedPixel = event.target;
   const selectedColor = document.getElementsByClassName('selected')[0];
-  if (event.target.style.backgroundColor !== selectedColor.style.backgroundColor) {
-    event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+  if (clickedPixel.style.backgroundColor !== selectedColor.style.backgroundColor) {
+    clickedPixel.style.backgroundColor = selectedColor.style.backgroundColor;
   }
 }
 
-for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener('click', turnSameColor);
+function createPixelBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
+  if (boardInput.value === '') {
+    alert('Board inválido!');
+  }
+  removeLastBoard();
+  for (let index = 0; index < boardInput.value; index += 1) {
+    const tRow = document.createElement('tr');
+    pixelBoard.appendChild(tRow);
+    for (let index2 = 0; index2 < boardInput.value; index2 += 1) {
+      const tDiv = document.createElement('td');
+      tDiv.className = 'pixel';
+      tDiv.style.backgroundColor = 'white';
+      tDiv.addEventListener('click', turnSameColor);
+      pixelBoard.children[index].appendChild(tDiv);
+    }
+  }
+  boardInput.value = '';
 }
 
-function eraserBoard(event) {
+function eraserBoard() {
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].style.backgroundColor = 'white';
   }
 }
 
-const button = document.getElementById('clear-board');
-button.addEventListener('click', eraserBoard);
+function maxMinValue() {
+  if (boardInput.value < 5 && boardInput.value !== '') {
+    boardInput.value = 5;
+  } else if (boardInput.value > 50) {
+    boardInput.value = 50;
+  }
+}
+
+eraseBtn.addEventListener('click', eraserBoard);
+boardBtn.addEventListener('click', maxMinValue);
+boardBtn.addEventListener('click', createPixelBoard);
+
+function onLoadSite() {
+  turnBlackSelected();
+  createPixelBoard();
+}
+
+window.onload = onLoadSite;
